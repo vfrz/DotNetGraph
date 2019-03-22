f3e707c5d9d789e7576d281d7ccaa5f112d95422
Pavel Vishnyakov <pavel.vishnyakov@asml.com>
2019-03-22 09:01:02 +0100

    add fontcolor and color attributes to arrows

--- Sources/DotNetGraph/DotArrow.cs
+++ Sources/DotNetGraph/DotArrow.cs
@@ -27,6 +27,15 @@ namespace DotNetGraph
         [DotAttribute("arrowhead", DotArrowShape.Normal)]
         public DotArrowShape ArrowHeadShape { get; set; } = DotArrowShape.Normal;
 
+        [DotAttribute("color", DotColor.Black)]
+        public DotColor ArrowColor { get; set; } = DotColor.Black;
+
+        [DotAttribute("fontcolor", DotColor.Black)]
+        public DotColor FontColor { get; set; } = DotColor.Black;
+
+        [DotAttribute("label", "\n")]
+        public string ArrowLabel { get; set; } = "\n";
+
         public DotArrow(string startNodeName, string targetNodeName)
         {
             StartNodeName = startNodeName;

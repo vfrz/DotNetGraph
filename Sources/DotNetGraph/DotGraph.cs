6d3c7ee80db90a6e328b73e2fc0cd5703bf4ca0c
Valentin <fritz.valentin@live.fr>
2019-12-11 21:33:51 +0100

    Clean

--- Sources/DotNetGraph/DotGraph.cs
+++ Sources/DotNetGraph/DotGraph.cs
@@ -1,22 +1,4 @@
-﻿/*
- * Copyright (c) 2019 DotNetGraph https://github.com/vfrz/DotNetGraph
- *  This file is part of DotNetGraph.
- *
- *     DotNetGraph is free software: you can redistribute it and/or modify
- *     it under the terms of the GNU General Public License as published by
- *     the Free Software Foundation, either version 3 of the License, or
- *     (at your option) any later version.
- *
- *     DotNetGraph is distributed in the hope that it will be useful,
- *     but WITHOUT ANY WARRANTY; without even the implied warranty of
- *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *     GNU General Public License for more details.
- *
- *     You should have received a copy of the GNU General Public License
- *     along with DotNetGraph.  If not, see <http://www.gnu.org/licenses/>.
- */
-
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Globalization;
 using System.Linq;
@@ -50,7 +32,7 @@ namespace DotNetGraph
                 if (element is DotNode node)
                     builder.Append((minified ? "" : "\t") + node.Name + " [ ");
                 else if (element is DotArrow arrow)
-                    if (Directed == true)
+                    if (Directed)
                         builder.Append((minified ? "" : "\t") + arrow.StartNodeName + "->" + arrow.TargetNodeName + " [ ");
                     else
                         builder.Append((minified ? "" : "\t") + arrow.StartNodeName + "--" + arrow.TargetNodeName + " [ ");
